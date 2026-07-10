cbfe2a4df63178b334b7e10f9e1a02e56dff584b
Mahnoor Fatima <185599930+Mahnoor-fatima249@users.noreply.github.com>
2026-07-11 00:23:23 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.config.database import (
+    Base,
+    engine
+)
+
+from app.routes.auth import router as auth_router
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title="AI Study Assistant API"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth_router)
+
+
+@app.get("/")
+def root():
+    return {
+        "message":
+        "AI Study Assistant API Running"
+    }
